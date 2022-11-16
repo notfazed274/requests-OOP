2fdbcf83d20ba8d20228030c3480688e59f28371
Federico <fpalacio@kindalab.co>
2022-11-16 11:53:13 -0300

    Moved json data to utils file

---
+++ tests/utils/endpoints.py
@@ -0,0 +1,17 @@
+import json
+
+books_api_status = 'https://simple-books-api.glitch.me/status'
+books_api_list_books = 'https://simple-books-api.glitch.me/books'
+books_api_auth = 'https://simple-books-api.glitch.me/api-clients'
+
+
+authorization = json.dumps(
+    {
+      "clientName": "F",
+      "clientEmail": "FP@abc.com"
+     }
+    )
+
+
+head = json.dumps({"Content-Type": "application/json"})
+
