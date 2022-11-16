597ca4c3d1d90db23f17c19a68b754b54792a5b8
Federico <fpalacio@kindalab.co>
2022-11-16 14:20:34 -0300

    Modified test suite

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,20 +3,20 @@ from utils.endpoints import *
 
 
 def test_api_books_status():
-    list_of_books = requestMethods(books_api_list_books, '', '')
+    list_of_books = requestMethods(books_api_list_books, headers, '')
     status_code = list_of_books.get()
     assert status_code == 200
     print(list_of_books.test)
 
 
 def test_api_books_response():
-    list_of_books = requestMethods(books_api_list_books, '', '')
+    list_of_books = requestMethods(books_api_list_books, headers, '')
     response = list_of_books.get_response()
     print(response)
 
 
 def test_api_books_post():
-    list_of_books = requestMethods(books_api_auth, head, authorization)
+    list_of_books = requestMethods(books_api_auth, headers, authorization)
     response = list_of_books.post()
     assert response[1] == 200
     print(response)
