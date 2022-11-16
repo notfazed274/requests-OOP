0d47504157b87a7388ac7e6a2054e8501bf3bf0b
Federico <fpalacio@kindalab.co>
2022-11-16 15:11:37 -0300

    Add comments

--- tests/utils/endpoints.py
+++ tests/utils/endpoints.py
@@ -1,17 +1,17 @@
 import json
 
+# API books endpoints
 books_api_status = 'https://simple-books-api.glitch.me/status'
 books_api_list_books = 'https://simple-books-api.glitch.me/books'
 books_api_auth = 'https://simple-books-api.glitch.me/api-clients'
 
-
+# Json for requests
 authorization = json.dumps(
     {
       "clientName": "F",
       "clientEmail": "FP@abc.com"
      }
     )
-
 
 headers = json.dumps({"Content-Type": "application/json"})
 
