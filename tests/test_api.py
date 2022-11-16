49fbc50120bd80916f384e800c14c89c33ef60e4
Federico <fpalacio@kindalab.co>
2022-11-16 11:52:12 -0300

    Added POST test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,22 +1,32 @@
 from utils.reqClasses import requestMethods
-import json
-
-# Should exclude url from test suite
-cats_api = 'https://http.cat/200'
-
-h = open('commonJson.json')
-headers = json.load(h)    
+from utils.endpoints import *
 
 
-def test_get():
-    list_of_cats = requestMethods(cats_api,'', '')
-    response = list_of_cats.get()
+def test_api_books_status():
+    list_of_books = requestMethods(books_api_list_books, '', '')
+    status_code = list_of_books.get()
+    assert status_code == 200
+    print(list_of_books.test)
+
+
+def test_api_books_response():
+    list_of_books = requestMethods(books_api_list_books, '', '')
+    response = list_of_books.get_response()
     print(response)
 
-def test_another_get():
-    list_of_cats = requestMethods(cats_api, '', '')
-    response = list_of_cats.get_response()
+
+def test_api_books_post():
+    list_of_books = requestMethods(books_api_auth, head, authorization)
+    response = list_of_books.post()
+    assert response[1] == 200
     print(response)
+
+
+
+
+
+
+
 
 
 
