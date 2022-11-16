d25b670e04ef107bc9a01babcc0e47e871cce42e
Federico <fpalacio@kindalab.co>
2022-11-16 15:12:12 -0300

    Modified second test

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,14 +12,15 @@ def test_api_books_status():
 
 def test_api_books_response():
     list_of_books = requestMethods(books_api_list_books, headers, '')
+    # response = dict //
     response = list_of_books.get_response()
-    print(response)
+    print(response[0])
 
 
 def test_api_books_post():
     list_of_books = requestMethods(books_api_auth, headers, authorization)
     response = list_of_books.post()
-    assert response[1] == 200
+    assert response[0] == 200
     print(response)
 
 
